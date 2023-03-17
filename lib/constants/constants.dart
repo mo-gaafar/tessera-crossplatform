4de67b8c9457b7a5dfcd17adf30afd8ddab8c89d
Zeyad <zeyadmansour2@gmail.com>
2023-03-17 19:41:26 +0200

    theme cubit test + theme tweaks

--- lib/constants/constants.dart
+++ lib/constants/constants.dart
@@ -2,4 +2,8 @@ import 'package:flutter/material.dart';
 
 class AppColors {
   static const Color primary = Color(0xffd1410c);
+  static const Color secondary = Color(0xff3659e3);
+  static Color lightBackground = Colors.grey.shade200;
+  static const Color textOnLight = Color(0xff1e0a3c);
+  static const Color secondaryTextOnLight = Color(0xff39364f);
 }
--- lib/core/theme/app_theme.dart
+++ lib/core/theme/app_theme.dart
@@ -4,14 +4,22 @@ import 'package:tessera/constants/constants.dart';
 ThemeData lightTheme = ThemeData.light().copyWith(
   colorScheme: const ColorScheme.light().copyWith(
     primary: AppColors.primary,
-    secondary: AppColors.primary,
+    secondary: AppColors.secondary,
+    onSecondary: Colors.white,
   ),
+  textTheme: ThemeData.light().textTheme.apply(
+        fontFamily: 'NeuePlak',
+        bodyColor: AppColors.textOnLight,
+        // displayColor: AppColors.secondaryTextOnLight,
+      ),
+  scaffoldBackgroundColor: AppColors.lightBackground,
 );
 
 ThemeData darkTheme = ThemeData.dark().copyWith(
   colorScheme: const ColorScheme.dark().copyWith(
     surface: AppColors.primary,
     primary: AppColors.primary,
-    secondary: AppColors.primary,
+    secondary: AppColors.secondary,
   ),
+  textTheme: ThemeData.dark().textTheme.apply(fontFamily: 'NeuePlak'),
 );
