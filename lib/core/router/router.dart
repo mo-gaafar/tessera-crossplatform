0b439734cea0a298326792fd414a72bef48bcada
Zeyad <zeyadmansour2@gmail.com>
2023-03-18 15:22:36 +0200

    example screen

--- lib/core/router/router.dart
+++ lib/core/router/router.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:tessera/features/example/view/pages/example_screen.dart';
 
 class AppRouter {
   Route onGenerateRoute(RouteSettings settings) {
     switch (settings.name) {
       case '/':
         return MaterialPageRoute(
-          builder: (_) => Container(),
+          builder: (_) => const ExampleScreen(),
         );
       case '/second':
         return MaterialPageRoute(
