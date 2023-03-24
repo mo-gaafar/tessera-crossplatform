75887b588e7ac3d007d2157381a5458d5c318a09
Zeyad <zeyadmansour2@gmail.com>
2023-03-24 23:41:17 +0200

    initial layout

--- lib/features/landing_page/view/pages/landing_page.dart
+++ lib/features/landing_page/view/pages/landing_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
 import 'package:tessera/constants/app_colors.dart';
+import 'package:tessera/features/landing_page/view/widgets/event_card.dart';
 import 'package:tessera/features/landing_page/view/widgets/nearby_events_section.dart';
 
 class LandingPage extends StatelessWidget {
@@ -10,41 +11,88 @@ class LandingPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        elevation: 0,
-        title: const Text(
-          'Tessera',
-          style: TextStyle(fontSize: 40, fontFamily: 'NeuePlak-Extended'),
-        ),
-      ),
       backgroundColor: AppColors.primary,
-      body: Padding(
-        padding: const EdgeInsets.only(top: 60),
-        child: Stack(
-          alignment: Alignment.topCenter,
-          children: [
-            const Text(
-              // 'Restless?',
-              // 'Looking for a new thrill?',
-              // 'Don't know how to spend your weekend?',
-              'Need that adrenaline rush?',
-              style: TextStyle(fontSize: 22, color: Colors.white),
+      body: CustomScrollView(
+        slivers: [
+          const SliverAppBar(
+            elevation: 0,
+            // backgroundColor: Colors.black,
+            pinned: true,
+            // title: Text(
+            //   'Tessera',
+            //   style: TextStyle(fontSize: 40, fontFamily: 'NeuePlak-Extended'),
+            // ),
+            flexibleSpace: SafeArea(
+              child: FlexibleSpaceBar(
+                centerTitle: true,
+                title: Text(
+                  'Tessera',
+                  style:
+                      TextStyle(fontSize: 40, fontFamily: 'NeuePlak-Extended'),
+                ),
+                // background: Container(
+                //   alignment: Alignment.center,
+                //   color: AppColors.primary,
+                //   child: Text(
+                //     // 'Restless?',
+                //     // 'Looking for a new thrill?',
+                //     // 'Don't know how to spend your weekend?',
+                //     'Need that adrenaline rush?',
+                //     style: TextStyle(fontSize: 22, color: Colors.white),
+                //   ),
+                // ),
+              ),
             ),
-            Container(
-              padding: const EdgeInsets.all(20),
-              alignment: Alignment.topLeft,
-              margin: const EdgeInsets.only(top: 90),
-              width: double.maxFinite,
-              height: double.maxFinite,
-              decoration: BoxDecoration(
-                  borderRadius:
-                      const BorderRadius.vertical(top: Radius.circular(30)),
-                  color: AppColors.lightBackground),
-              child: const NearbyEvents(),
-            ),
-          ],
-        ),
+            expandedHeight: 200,
+          ),
+          NearbyEvents(title: 'Events Near New Cairo'),
+          NearbyEvents(title: 'Events We Think You\'ll Love!', radius: 0),
+        ],
       ),
+      // appBar: AppBar(
+      //   elevation: 0,
+      //   title: const Text(
+      //     'Tessera',
+      //     style: TextStyle(fontSize: 40, fontFamily: 'NeuePlak-Extended'),
+      //   ),
+      // ),
+      // backgroundColor: AppColors.primary,
+      // body: ListView(
+      //   clipBehavior: Clip.none,
+      //   children: [
+      //     const Text(
+      //       // 'Restless?',
+      //       // 'Looking for a new thrill?',
+      //       // 'Don't know how to spend your weekend?',
+      //       'Need that adrenaline rush?',
+      //       style: TextStyle(fontSize: 22, color: Colors.white),
+      //     ),
+      //     Container(
+      //       clipBehavior: Clip.none,
+      //       padding: const EdgeInsets.all(20),
+      //       alignment: Alignment.topLeft,
+      //       // margin: const EdgeInsets.only(top: 90),
+      //       decoration: BoxDecoration(
+      //           borderRadius:
+      //               const BorderRadius.vertical(top: Radius.circular(30)),
+      //           color: AppColors.lightBackground),
+      //       child: Column(
+      //         mainAxisSize: MainAxisSize.min,
+      //         children: List.generate(
+      //             10,
+      //             (index) => Padding(
+      //                   padding: const EdgeInsets.symmetric(vertical: 5),
+      //                   child: EventCard(
+      //                       eventTitle: 'The Weeknd Tour',
+      //                       eventImage:
+      //                           Image.asset('assets/images/placeholder.jpg'),
+      //                       eventDate: '12/12/2021',
+      //                       eventLocation: 'Koshk Omar Cultural Center'),
+      //                 )),
+      //       ),
+      //     ),
+      //   ],
+      // ),
     );
   }
 }
--- lib/features/landing_page/view/widgets/nearby_events_section.dart
+++ lib/features/landing_page/view/widgets/nearby_events_section.dart
@@ -1,24 +1,111 @@
 import 'package:flutter/material.dart';
+import 'package:tessera/constants/app_colors.dart';
 import 'package:tessera/features/landing_page/view/widgets/event_card.dart';
+import 'package:sliver_tools/sliver_tools.dart';
 
 class NearbyEvents extends StatelessWidget {
-  const NearbyEvents({super.key});
+  NearbyEvents({
+    super.key,
+    required this.title,
+    this.radius = 20,
+  });
+
+  final String title;
+  final double radius;
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
+    return SliverStack(
       children: [
-        const Text(
-          'Events Near New Cairo',
-          style: TextStyle(fontSize: 20),
+        SliverPositioned.fill(
+          child: Container(
+              clipBehavior: Clip.hardEdge,
+              decoration: BoxDecoration(
+                color: AppColors.lightBackground,
+                borderRadius: BorderRadius.vertical(
+                  top: Radius.circular(radius),
+                ),
+              )),
         ),
-        EventCard(
-            eventTitle: 'The Weeknd Tour',
-            eventImage: Image.asset('assets/images/placeholder.jpg'),
-            eventDate: '12/12/2021',
-            eventLocation: 'Koshk Omar Cultural Center'),
+        SliverPadding(
+          padding: const EdgeInsets.all(10),
+          sliver: MultiSliver(
+            pushPinnedChildren: true,
+            children: [
+              SliverPinnedHeader(
+                child: ColoredBox(
+                  color: AppColors.lightBackground,
+                  child: ListTile(
+                    title: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          title,
+                          style: TextStyle(fontSize: 25),
+                        ),
+                        // FilterChip(
+                        //   selected: false,
+                        //   label: Text('text'),
+                        //   onSelected: (value) {},
+                        //   showCheckmark: false,
+                        // )
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+              SliverList(
+                delegate: SliverChildBuilderDelegate(
+                  (context, index) => Padding(
+                    padding: const EdgeInsets.only(bottom: 10),
+                    child: EventCard(
+                        eventTitle: 'The Weeknd Tour',
+                        eventImage:
+                            Image.asset('assets/images/placeholder.jpg'),
+                        eventDate: '12/12/2021',
+                        eventLocation: 'Koshk Omar Cultural Center'),
+                  ),
+                  childCount: 10,
+                ),
+              ),
+            ],
+          ),
+        ),
       ],
     );
   }
 }
+
+// class NearbyEvents extends MultiSliver {
+//   NearbyEvents({
+//     Key? key,
+//     required String title,
+//     Color headerColor = Colors.white,
+//     Color titleColor = Colors.black,
+//     // required List<Widget> items,
+//   }) : super(
+//           key: key,
+//           pushPinnedChildren: true,
+//           children: [
+//             SliverPinnedHeader(
+//               child: ColoredBox(
+//                 color: headerColor,
+//                 child: ListTile(
+//                   textColor: titleColor,
+//                   title: Text(title),
+//                 ),
+//               ),
+//             ),
+//             SliverList(
+//               delegate: SliverChildBuilderDelegate(
+//                 (context, index) => EventCard(
+//                     eventTitle: 'The Weeknd Tour',
+//                     eventImage: Image.asset('assets/images/placeholder.jpg'),
+//                     eventDate: '12/12/2021',
+//                     eventLocation: 'Koshk Omar Cultural Center'),
+//                 childCount: 10,
+//               ),
+//             ),
+//           ],
+//         );
+// }
