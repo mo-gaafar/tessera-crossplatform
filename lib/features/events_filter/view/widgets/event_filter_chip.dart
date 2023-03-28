cb6d5a87b417db687fa96c5eb91329929f8138f4
Zeyad Mansour <zeyadmansour2@gmail.com>
2023-03-29 00:58:22 +0200

    filter chip appearance adjustments

--- lib/features/events_filter/view/widgets/event_filter_chip.dart
+++ lib/features/events_filter/view/widgets/event_filter_chip.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:tessera/constants/app_colors.dart';
 import 'package:tessera/features/events_filter/cubit/events_filter_cubit.dart';
 import 'package:tessera/features/events_filter/data/filter_criteria_model.dart';
 
@@ -17,13 +18,25 @@ class _EventFilterChipState extends State<EventFilterChip> {
   // final FilterCriterion filterCriterion;
   @override
   Widget build(BuildContext context) {
-    return FilterChip(
-      label: Text(widget.label),
-      selected: widget.isSelected,
-      onSelected: (value) {
-        setState(() => widget.isSelected = !widget.isSelected);
-        context.read<EventsFilterCubit>().onSelectionChanged();
-      },
+    return Padding(
+      padding: const EdgeInsets.only(right: 5),
+      child: FilterChip(
+        // shape:
+        //     const StadiumBorder(side: BorderSide(color: AppColors.secondary)),
+        padding: const EdgeInsets.symmetric(horizontal: 5),
+        label: Text(widget.label),
+        selected: widget.isSelected,
+        selectedColor: AppColors.secondary,
+        labelStyle: widget.isSelected
+            ? const TextStyle(color: Colors.white)
+            : const TextStyle(color: AppColors.secondaryTextOnLight),
+        // showCheckmark: false,
+        checkmarkColor: Colors.white.withOpacity(0.7),
+        onSelected: (value) {
+          setState(() => widget.isSelected = !widget.isSelected);
+          context.read<EventsFilterCubit>().onSelectionChanged();
+        },
+      ),
     );
   }
 }
