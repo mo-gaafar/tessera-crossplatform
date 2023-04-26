37954ecadb3d18b1be6bccc72f194ab6f93d88a9
Zeyad <zeyadmansour2@gmail.com>
2023-04-26 10:17:35 +0200

    login bug fixes merged

--- lib/features/authentication/view/pages/signup_screen.dart
+++ lib/features/authentication/view/pages/signup_screen.dart
@@ -20,58 +20,15 @@ class SignUp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     double kPagePadding = 20;
-    return SafeArea(
-      child: AppScaffold(
-        appBar: AppBar(
-          title: const Text(
-            "Sign up",
-            style: TextStyle(
-                fontFamily: 'NeuePlak',
-                fontSize: 25,
-                fontWeight: FontWeight.bold),
-          ),
+    return AppScaffold(
+      appBar: AppBar(
+        title: const Text(
+          "Sign up",
+          style: TextStyle(
+              fontFamily: 'NeuePlak',
+              fontSize: 25,
+              fontWeight: FontWeight.bold),
         ),
-        body: Padding(
-          padding: EdgeInsets.all(kPagePadding),
-          child: Form(
-            key: formkey,
-            child: Column(
-              children: [
-                Container(
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          const Text(
-                            'Email',
-                            style: TextStyle(color: Colors.grey, fontSize: 20),
-                          ),
-                          const SizedBox(
-                            height: 2,
-                          ),
-                          Text(
-                            context.read<AuthCubit>().currentUser.email,
-                            style: const TextStyle(color: Colors.grey),
-                          ),
-                          const SizedBox(
-                            height: 4,
-                          ),
-                          const Divider(
-                            color: Colors.grey,
-                            height: 2,
-                            thickness: 1,
-                          ),
-                        ],
-                      ),
-                      TextFormField(
-                        decoration: const InputDecoration(
-                          labelText: "Confirm Email",
-                          enabledBorder: UnderlineInputBorder(
-                            borderSide: BorderSide(color: Colors.grey),
-                          ),
-                          hintText: 'Confirm Email',
       ),
       body: Padding(
         padding: EdgeInsets.all(kPagePadding),
@@ -79,122 +36,119 @@ class SignUp extends StatelessWidget {
           key: formkey,
           child: Column(
             children: [
-              Container(
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        const Text(
-                          'Email',
-                          style: TextStyle(color: Colors.grey, fontSize: 20),
-                        ),
-                        const SizedBox(
-                          height: 2,
-                        ),
-                        Text(
-                          context.read<AuthCubit>().currentUser.email,
-                          style: const TextStyle(color: Colors.grey),
-                        ),
-                        const SizedBox(
-                          height: 4,
-                        ),
-                        const Divider(
-                          color: Colors.grey,
-                          height: 2,
-                          thickness: 1,
-                        ),
-                      ],
-                    ),
-                    TextFormField(
-                      decoration: const InputDecoration(
-                        labelText: "Confirm Email",
-                        enabledBorder: UnderlineInputBorder(
-                          borderSide: BorderSide(color: Colors.grey),
-                        ),
-                        hintText: 'Confirm Email',
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      const Text(
+                        'Email',
+                        style: TextStyle(color: Colors.grey, fontSize: 20),
                       ),
-                      validator: (value) {
-                        if (value!.trim().isEmpty) {
-                          return 'Please re enter your email to confirm it';
-                        }
-                        if (value !=
-                            context.read<AuthCubit>().currentUser.email) {
-                          return 'Email must be the same';
-                        }
-                      },
-                    ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                      children: [
-                        Expanded(
-                          child: TextFormField(
-                            decoration: const InputDecoration(
-                              labelText: "First Name",
-                              enabledBorder: UnderlineInputBorder(
-                                borderSide: BorderSide(color: Colors.grey),
-                              ),
-                              hintText: 'Enter First Name',
-                            ),
-                            validator: (value) {
-                              _firstName = value!;
-                              if (_firstName.trim().isEmpty) {
-                                return 'First name is required';
-                              }
-                            },
-                          ),
-                        ),
-                        const SizedBox(width: 15),
-                        Expanded(
-                          child: TextFormField(
-                            decoration: const InputDecoration(
-                              labelText: "Surname",
-                              enabledBorder: UnderlineInputBorder(
-                                borderSide: BorderSide(color: Colors.grey),
-                              ),
-                              hintText: 'Enter Surname',
-                            ),
-                            validator: (value) {
-                              _lastName = value!;
-                              if (_lastName.trim().isEmpty) {
-                                return 'Surname is required';
-                              }
-                            },
-                          ),
-
-                        ],
+                      const SizedBox(
+                        height: 2,
                       ),
-                      PasswordFormField(formValidator: formValidator),
+                      Text(
+                        context.read<AuthCubit>().currentUser.email,
+                        style: const TextStyle(color: Colors.grey),
+                      ),
+                      const SizedBox(
+                        height: 4,
+                      ),
+                      const Divider(
+                        color: Colors.grey,
+                        height: 2,
+                        thickness: 1,
+                      ),
                     ],
                   ),
-                ),
-                const Spacer(),
-                Container(
-                  padding: const EdgeInsets.only(top: 10),
-                  width: double.infinity,
-                  child: EmailButton(
-                    buttonText: 'Sign Up',
-                    colourBackground: AppColors.primary,
-                    colourText: Colors.white,
-                    onTap: () async {
-                      if (formkey.currentState!.validate()) {
-                        // Attempt email sign up
-                        String tempVarPassword=context.read<AuthCubit>().currentUser.password!;
-                        await context
-                            .read<AuthCubit>()
-                            .emailSignUp(_firstName, _lastName, tempVarPassword);
-
-                        if (context.read<AuthCubit>().state is EmailSignedUp) {
-                          Navigator.pushNamed(context, '/verification');
-                        }
-
+                  TextFormField(
+                    decoration: const InputDecoration(
+                      labelText: "Confirm Email",
+                      enabledBorder: UnderlineInputBorder(
+                        borderSide: BorderSide(color: Colors.grey),
+                      ),
+                      hintText: 'Confirm Email',
+                    ),
+                    validator: (value) {
+                      if (value!.trim().isEmpty) {
+                        return 'Please re enter your email to confirm it';
+                      }
+                      if (value !=
+                          context.read<AuthCubit>().currentUser.email) {
+                        return 'Email must be the same';
                       }
                     },
                   ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      Expanded(
+                        child: TextFormField(
+                          decoration: const InputDecoration(
+                            labelText: "First Name",
+                            enabledBorder: UnderlineInputBorder(
+                              borderSide: BorderSide(color: Colors.grey),
+                            ),
+                            hintText: 'Enter First Name',
+                          ),
+                          validator: (value) {
+                            _firstName = value!;
+                            if (_firstName.trim().isEmpty) {
+                              return 'First name is required';
+                            }
+                          },
+                        ),
+                      ),
+                      const SizedBox(width: 15),
+                      Expanded(
+                        child: TextFormField(
+                          decoration: const InputDecoration(
+                            labelText: "Surname",
+                            enabledBorder: UnderlineInputBorder(
+                              borderSide: BorderSide(color: Colors.grey),
+                            ),
+                            hintText: 'Enter Surname',
+                          ),
+                          validator: (value) {
+                            _lastName = value!;
+                            if (_lastName.trim().isEmpty) {
+                              return 'Surname is required';
+                            }
+                          },
+                        ),
+                      ),
+                    ],
+                  ),
+                  PasswordFormField(formValidator: formValidator),
+                ],
+              ),
+              const Spacer(),
+              Container(
+                padding: const EdgeInsets.only(top: 10),
+                width: double.infinity,
+                child: EmailButton(
+                  buttonText: 'Sign Up',
+                  colourBackground: AppColors.primary,
+                  colourText: Colors.white,
+                  onTap: () async {
+                    if (formkey.currentState!.validate()) {
+                      // Attempt email sign up
+                      String tempVarPassword =
+                          context.read<AuthCubit>().currentUser.password!;
+                      await context
+                          .read<AuthCubit>()
+                          .emailSignUp(_firstName, _lastName, tempVarPassword);
+
+                      if (context.read<AuthCubit>().state is EmailSignedUp) {
+                        Navigator.pushNamed(context, '/verification');
+                      }
+                    }
+                  },
                 ),
-              ],
-            ),
+              ),
+            ],
           ),
         ),
       ),
